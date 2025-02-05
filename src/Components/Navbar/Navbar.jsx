f365b1009bfde663e2bd73ba94a6d90531d19357
vanshsharma06 <sharmavansh932@gmail.com>
2025-02-05 23:04:51 +0530

    fix

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,55 @@
-// import React from "react";
-import { FaPhoneAlt } from "react-icons/fa";
+import { useState } from "react";
+import { FaPhoneAlt, FaBars, FaTimes } from "react-icons/fa";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Sidebar toggle function
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <header>
         <nav>
           <div className={styles.navbar}>
-            <div className={styles.left}>LOGO</div>
-            <div className={styles.mid}>
-              <ul>
-                <li>Home</li>
-                <li>Listings</li>
-                <li>Blog</li>
-                <li>Contact</li>
-              </ul>
+            {/* Left Section: Hamburger + Logo */}
+            <div className={styles.leftSection}>
+              <div className={styles.hamburger} onClick={toggleSidebar}>
+                {isOpen ? <FaTimes /> : <FaBars />}
+              </div>
+              <div className={styles.logo}>LOGO</div>
             </div>
+
+            {/* Normal Navbar */}
+            {!isOpen && (
+              <div className={styles.mid}>
+                <ul>
+                  <li>Home</li>
+                  <li>Listings</li>
+                  <li>Blog</li>
+                  <li>Contact</li>
+                </ul>
+              </div>
+            )}
+
+            {/* Right Section */}
             <div className={styles.right}>
               <FaPhoneAlt className={styles.icon} />
               <button>Add Property</button>
             </div>
+          </div>
+
+          {/* Sidebar */}
+          <div className={`${styles.sidebar} ${isOpen ? styles.show : ""}`}>
+            <ul>
+              <li onClick={toggleSidebar}>Home</li>
+              <li onClick={toggleSidebar}>Listings</li>
+              <li onClick={toggleSidebar}>Blog</li>
+              <li onClick={toggleSidebar}>Contact</li>
+            </ul>
           </div>
         </nav>
       </header>
