0d1cd09575db163c0d4c35a4caacd48ef280ad78
vanshsharma06 <sharmavansh932@gmail.com>
2025-02-05 11:57:05 +0530

    Your commit message here

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// import React from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Navbar from "./Components/Navbar/Navbar";
+import Container from "./Components/Container/Container";
+import WhyUs from "./Components/WhyUS/WhyUS";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FeaturedProperties from "./Components/FeaturedProperties/FeaturedProperties";
+import AllProperties from "./Components/FeaturedProperties/AllProperties";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Container></Container>
+      <Router>
+        <Routes>
+          <Route path="/" element={<FeaturedProperties />} />
+          <Route path="/all-properties" element={<AllProperties />} />
+        </Routes>
+      </Router>
+      <WhyUs></WhyUs>
+    </div>
+  );
+}
